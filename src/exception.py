9aadc96737f8285c25eff3b352d35b6876b36b9e
Dinesh Ram <dnshram013.@gmail.com>
2024-04-17 12:30:39 +0530

    commit.update

--- src/exception.py
+++ src/exception.py
@@ -5,7 +5,6 @@ def error_message_details(error,error_detail:sys):
     _,_,exc_tb=error_detail.exc_info()
     file_name=exc_tb.tb_frame.f_code.co_filename
     error_message="Error occured in python script [{0}] line number [{1}] file name [{2}]".format(file_name,exc_tb.tb_lineno,str(error))
-
     return error_message()
 
 class CustomException(Exception):
